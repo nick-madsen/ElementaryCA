eea520ea95c7b415ac201b84eeefd5fe9969b0a2
nick-madsen <nick.madsen15@gmail.com>
2018-04-04 11:42:00 -0400

    Started Program
    Started learning what the heck I'm doing and setting up the code.

---
+++ ElementaryCA/src/ryugoAndMadsen/ElementaryCA.java
@@ -0,0 +1,18 @@
+package ryugoAndMadsen;
+
+public class ElementaryCA {
+	private static void nextGen(int[] field, int rule) {
+		int nextField[] = new int[field.length];
+		for (int x = 0; x < field.length; x++) {
+			if (x == 0) {
+				
+			}
+		}
+	}
+	private static int[] field() {
+		
+	}
+	public static void main() {
+		
+	}
+}
